b99c432950058a43a9babab175a81d504ea79363
Alex30340 <alex.navarro30340@gmail.com>
2025-03-28 00:11:22 +0100

    Update app_instance.py

--- core/app_instance.py
+++ core/app_instance.py
@@ -4,6 +4,7 @@ import dash_bootstrap_components as dbc
 app = Dash(
     __name__,
     use_pages=True,
+    pages_folder="pages",  # <-- ajoute ce paramètre pour forcer Dash à voir le bon dossier
     external_stylesheets=[dbc.themes.BOOTSTRAP],
     suppress_callback_exceptions=True
 )
