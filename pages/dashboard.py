e2f6b856d34c7c63e48772decd44542d8a925242
Alex30340 <alex.navarro30340@gmail.com>
2025-03-28 22:00:08 +0100

    Update dashboard.py

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -1,1 +1,9 @@
+# pages/dashboard.py
+
+import streamlit as st
+
+def run():
+    st.title("Portefeuille")
+    st.write("Suivi des performances et positions")
+    # Tu peux rajouter ici ton code graphique ou CSV
 # placeholder
