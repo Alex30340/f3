ffd05b0c4a997ad970923dd51f84b50be1179a32
Alex30340 <alex.navarro30340@gmail.com>
2025-03-28 21:44:01 +0100

    Add files via upload

---
+++ pages/app.py
@@ -0,0 +1,18 @@
+# app.py — version Streamlit
+
+import streamlit as st
+from pages import analyse, dashboard, backtest, education, lab
+
+st.set_page_config(page_title="Forex Analyzer", layout="wide")
+
+pages = {
+    "Analyse Technique": analyse.run,
+    "Portefeuille": dashboard.run,
+    "Backtest": backtest.run,
+    "Éducation": education.run,
+    "LAB": lab.run
+}
+
+st.sidebar.title("Navigation")
+choix = st.sidebar.radio("Aller vers :", list(pages.keys()))
+pages[choix]()
